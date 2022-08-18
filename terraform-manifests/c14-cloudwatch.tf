cba500b552a80f4acfc913b970d86005a081e582
AWSIaCDemo <90651730+AWSIaCDemo@users.noreply.github.com>
2022-08-18 13:55:02 -0400

    Update c14-cloudwatch.tf

--- terraform-manifests/c14-cloudwatch.tf
+++ terraform-manifests/c14-cloudwatch.tf
@@ -400,34 +400,34 @@ EOF
 
 }
 
-resource "aws_sns_topic" "cloud_watch_alarm_topic" {
-  count        = length(var.alarm_type)
-  name         = "cloud-watch-${var.alarm_type[count.index]}-alarm-topic"
-  display_name = "CW Alarm | ${var.alarm_type[count.index]} | ${terraform.workspace}"
-  tags         = local.common_tags
-}
+#resource "aws_sns_topic" "cloud_watch_alarm_topic" {
+# count        = length(var.alarm_type)
+#  name         = "cloud-watch-${var.alarm_type[count.index]}-alarm-topic"
+#  display_name = "CW Alarm | ${var.alarm_type[count.index]} | ${terraform.workspace}"
+#  tags         = local.common_tags
+#}
 
-resource "aws_sns_topic_subscription" "cloud_watch_alarm_email_target" {
-  count     = length(var.alarm_type)
-  topic_arn = aws_sns_topic.cloud_watch_alarm_topic[count.index].arn
-  protocol  = "email"
-  endpoint  = "AWS-CloudWatch-prlearn85@gmail.com" 
-} 
+#resource "aws_sns_topic_subscription" "cloud_watch_alarm_email_target" {
+#  count     = length(var.alarm_type)
+ # topic_arn = aws_sns_topic.cloud_watch_alarm_topic[count.index].arn
+#  protocol  = "email"
+#  endpoint  = "AWS-CloudWatch-prlearn85@gmail.com" 
+#} 
 
-resource "aws_cloudwatch_metric_alarm" "cw_alarm" {
-  count                     = length(var.alarm_type)
-  alarm_name                = "${var.alarm_type[count.index]}-alarm"
-  comparison_operator       = "GreaterThanOrEqualToThreshold"
-  evaluation_periods        = "2"
-  metric_name               = var.metric_name[count.index] 
-  namespace                 = var.name_space[count.index]
-  period                    = var.period[count.index]
-  statistic                 = var.statistic[count.index]
-  threshold                 = var.threshold[count.index]
-  alarm_description         = "This metric monitors ${var.alarm_type[count.index]}"
-  insufficient_data_actions = []
+#resource "aws_cloudwatch_metric_alarm" "cw_alarm" {
+#  count                     = length(var.alarm_type)
+ # alarm_name                = "${var.alarm_type[count.index]}-alarm"
+ # comparison_operator       = "GreaterThanOrEqualToThreshold"
+#  evaluation_periods        = "2"
+#  metric_name               = var.metric_name[count.index] 
+#  namespace                 = var.name_space[count.index]
+#  period                    = var.period[count.index]
+#  statistic                 = var.statistic[count.index]
+ # threshold                 = var.threshold[count.index]
+ # alarm_description         = "This metric monitors ${var.alarm_type[count.index]}"
+ # insufficient_data_actions = []
   
-  actions_enabled           = "true"
-  alarm_actions             = [aws_sns_topic.cloud_watch_alarm_topic[count.index].arn]
-  tags                      = local.common_tags
-}
+#  actions_enabled           = "true"
+ # alarm_actions             = [aws_sns_topic.cloud_watch_alarm_topic[count.index].arn]
+ # tags                      = local.common_tags
+#}
